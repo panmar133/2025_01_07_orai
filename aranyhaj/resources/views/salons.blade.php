ad80ef3a7caa0a25a8b16fb22e33c2b96e0d0081
UraiZoe <urazoe366@hengersor.hu>
2025-02-05 11:26:52 +0100

    style

--- aranyhaj/resources/views/salons.blade.php
+++ aranyhaj/resources/views/salons.blade.php
@@ -12,34 +12,38 @@
 <!-- PHP-s kilistázás --> 
 
 
-    <div class="container">
-            <div class="row">
-                @foreach ($salons as $salon)
-                    <div class="col-12 col-md-6 col-lg-4 mb-4">
-                        <div class="card h-100">
-                            <div class="card-body" id="eventCards">
-                                <div class="row">
-                                    <div class="col-6">
-                                        <h5 class="card-title">{{ $salon->salon_name }}</h5>
-                                    </div>
-                                    <div class="col-6 text-end">
-                                        <p class="mb-1"><strong>Szalon tulajdonosa: </strong> {{$salon->owner->user_name ?? 'N/A'}}</p>
-                                    </div>
-                                </div>
-
-                                <img src="{{ asset('images/' . $salon->salon_name) }}" alt="Szalon Kép" class="img-fluid my-3">
-
-                                <p class="card-text">{{ $salon->information }}</p>
-                                <p class="card-text">
-                                    <strong>Location:</strong>
-                                    <a href="">{{ $salon->city }}, {{ $salon->street }}, {{ $salon->zip_code }}</a>
-                                </p>
+<div class="container">
+    <div class="row">
+        @foreach ($salons as $salon)
+            <div class="col-12 col-md-6 col-lg-4 mb-4">
+                <div class="card h-100">
+                    <div class="card-body d-flex flex-column" id="eventCards">
+                        <div class="row">
+                            <div class="col-6">
+                                <h5 class="card-title">{{ $salon->salon_name }}</h5>
+                            </div>
+                            <div class="col-6 text-end">
+                                <p class="mb-1"><strong>Szalon tulajdonosa: </strong> {{$salon->owner->user_name ?? 'N/A'}}</p>
                             </div>
                         </div>
+
+                        <img src="{{ asset('images/' . $salon->salon_name) }}" alt="Szalon Kép" class="img-fluid my-3">
+
+                        <p class="card-text">{{ $salon->information }}</p>
+
+                        <div class="mt-auto">
+                            <p class="card-text">
+                                <strong>Location:</strong>
+                                <a href="">{{ $salon->city }}, {{ $salon->street }}, {{ $salon->zip_code }}</a>
+                            </p>
+                        </div>
                     </div>
-                @endforeach
+                </div>
             </div>
-        </div>
+        @endforeach
+    </div>
+</div>
+
 </main>
 
 @endsection
