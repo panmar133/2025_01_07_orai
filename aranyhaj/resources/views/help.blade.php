0fcfdbac488f0f359359517b69f9941c2d87d216
UraiZoe <urazoe366@hengersor.hu>
2025-02-05 11:28:56 +0100

    oldal

---
+++ aranyhaj/resources/views/help.blade.php
@@ -0,0 +1,9 @@
+@extends("layouts.layout")
+<!-- Fejléc kiszedés -->
+@section("title", "Adományozok")
+<!-- Cím adás az oldalnak változó által -->
+@section("content")
+<!-- Kontent kiszedés -->
+ <h1>Gyakori kérdések</h1>
+@endsection
+<!-- Lezárás -->
