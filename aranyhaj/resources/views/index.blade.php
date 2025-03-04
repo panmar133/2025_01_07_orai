961058a0b3c915a5135b0d8f6d516151419035d9
UraiZoe <urazoe366@hengersor.hu>
2025-03-04 09:56:13 +0100

    sasdfghjkljhgfhdsfadSAíYSXDCGVFBJHNMK
    ayxcgfbhjmlékjuhtgfbaqY

--- aranyhaj/resources/views/index.blade.php
+++ aranyhaj/resources/views/index.blade.php
@@ -14,18 +14,26 @@
             <a href="/donate" class="btn btn-dark btn-sm d-block mx-auto">Adományozni szeretnék</a> <br>
         </div>
         
-        <div id="SecondContentMainPage" class="card px-3 px-md-5 mt-5 mb-5 mx-auto" style="max-width: 90%;">   
-            <h3>💇‍♀️<b>Miért fontos a hajadományozás?</b></h3>
-            <p> A parókák sokak számára nemcsak esztétikai, hanem érzelmi támogatást is jelentenek. <br>
-                Egy hajadomány nem csupán fizikai ajándék – ez egy új kezdet és egy mosoly forrása is lehet! </p>
-            <h3><b>🌟 Hogyan tudsz segíteni? </b></h3>
-            <ul>
-                <li><b>Adományozd le a hajad – </b>Tudj meg többet az adományozás feltételeiről! </li>
-                <li><b>Támogasd a kezdeményezést anyagilag –</b> Segítsd a paróka készítés költségeit! </li>
-                <li><b>Oszd meg a történetünket –</b> Minél többen tudnak róla, annál több emberhez érhet el a segítség! </li>
-            </ul>
+        <div id="SecondContentMainPage" class="card px-3 px-md-5 mt-5 mb-5 mx-auto" style="max-width: 90%;">
+            <div class="row align-items-center">
+                <div class="col-lg-8">
+                    <h3><b>Miért fontos a hajadományozás?</b></h3>
+                    <p>A parókák sokak számára nemcsak esztétikai, hanem érzelmi támogatást is jelentenek. <br>
+                        Egy hajadomány nem csupán fizikai ajándék – ez egy új kezdet és egy mosoly forrása is lehet! </p>
+                    <h3><b>Hogyan tudsz segíteni?</b></h3>
+                    <ul>
+                        <li><b>Adományozd le a hajad – </b>Tudj meg többet az adományozás feltételeiről!</li>
+                        <li><b>Támogasd a kezdeményezést anyagilag –</b> Segítsd a paróka készítés költségeit!</li>
+                        <li><b>Oszd meg a történetünket –</b> Minél többen tudnak róla, annál több emberhez érhet el a segítség!</li>
+                    </ul>
+                </div>
+                <div class="col-lg-4 text-center">
+                    <img id="girl" src="{{ asset('images/girl.png') }}" alt="Mosolygó lány" class="img-fluid rounded">
+                </div>
+            </div>
         </div>
-        
+
+
         <div id="ThirdContentMainPage" class="d-flex justify-content-center gap-4 flex-wrap">
             <div class="card text-center" style="width: 18rem;">
                 <a href="/events"><img src="{{ asset('images/event.png') }}" class="card-img-top" alt="events"></a>
