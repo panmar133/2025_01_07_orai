0bf0dd4aa806c5f78856ee3f738f5c2d49b7c062
UraiZoe <urazoe366@hengersor.hu>
2025-04-01 08:10:36 +0200

    Revert "last change"
    This reverts commit 2d6e6e93bc3d8ac47e5d20de03441c4e50be8146.

---
+++ aranyhaj/resources/views/layouts/footer.blade.php
@@ -0,0 +1,32 @@
+<footer>
+    <div id="footerLeft">
+        <h3>Közösségi Média</h3>
+        <ul>
+            <li><a href="https://www.facebook.com/aranyhajadomany/"><i class="fa-brands fa-facebook"></i>Facebook</a>
+            </li>
+            <li><a href="https://www.instagram.com"><i class="fa-brands fa-instagram"></i>Instagram</a></li>
+            <li><a href="https://www.tiktok.com"><i class="fa-brands fa-tiktok"></i>Tiktok</a></li>
+            <li><a href="https://www.youtube.com"><i class="fa-brands fa-youtube"></i>Youtube</a></li>
+        </ul>
+    </div>
+    <div id="footerCenter">
+        <h3>Hasznos linkek</h3>
+        <ul>
+            <li><a href="/user">Fiókom</a></li>
+            <li><a href="/donate">Adományozok</a></li>
+            <li><a href="/salons">Szalonok</a></li>
+            <li><a href="/help">Segítség</a></li>
+        </ul>
+    </div>
+    <div id="footerRight">
+        <h3>Elérhetőségünk</h3>
+        <ul>
+            <li id="contact"><i class="fa-solid fa-location-dot"></i>18.kerület Hengersor utca 39</li>
+            <li id="contact"><i class="fa-solid fa-envelope"></i>Aranyhaj@gmail.com</li>
+            <li id="contact"><i class="fa-solid fa-phone"></i>+36 70 000 0000</li>
+        </ul>
+    </div>
+</footer>
+</body>
+
+</html>
