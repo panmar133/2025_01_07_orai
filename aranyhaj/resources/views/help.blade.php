af12543b49578435045d05a62773613096641d4b
UraiZoe <urazoe366@hengersor.hu>
2025-04-10 21:51:34 +0200

    Segtség oldal

--- aranyhaj/resources/views/help.blade.php
+++ aranyhaj/resources/views/help.blade.php
@@ -38,19 +38,16 @@
                     <dd>A regisztrációhoz kattints a "Regisztráció" gombra, majd kövesd az ott megjelenő utasításokat.</dd>
 
                     <dt>Elfelejtettem a jelszavam, mit tegyek?</dt>
-                    <dd>Ha elfelejtetted a jelszavad, kattints a "Jelszó visszaállítása" linkre, és kövesd az e-mailben
-                        küldött utasításokat.
-                        Ha segítségre van szükséged, keresd ügyfélszolgálatunkat.</dd>
+                    <dd>Ha elfelejtetted a jelszavad, akkor kérj segítséget az adminisztrátoroktól.</dd>
 
                     <dt>Hogyan frissíthetem a fiókom adatait?</dt>
-                    <dd>Fiókod adatait a "Fiókom" oldalon frissítheted. Itt módosíthatod a lakcímedet, illetve a
+                    <dd>Fiókod adatait a "Fiókom" oldalon frissítheted. Itt módosíthatod a lakcímedet, profilképedet, illetve a
                         jelszavadat.</dd>
 
                     <dt>Miért nem tudok bejelentkezni a fiókomba?</dt>
                     <dd>Győződj meg róla, hogy helyesen adtad meg a felhasználóneved és a jelszavad. Ha továbbra is
                         problémát tapasztalsz,
-                        próbáld meg visszaállítani a jelszavad.</dd>
-
+                        kérj segítséget az adminisztrátoroktól.</dd>
                 </dl>
             </div>
         </div>
@@ -66,8 +63,7 @@
                     <dd>A jelentkezéshez navigálj az események oldalára, és kattints a "Részvételek" gombra.</dd>
 
                     <dt>Mi a teendő, ha le szeretném mondani a részvételemet?</dt>
-                    <dd>Jelenleg a "Részvétel" gomb csak a rendező számára jelzi, hogy érdeklődsz az esemény iránt,
-                        de a lemondásra nincs lehetőség. Ha mégis le szeretnéd mondani, vedd fel a kapcsolatot a rendezővel.
+                    <dd>Az adott eseményre nyomj rá mégegyszer a "Részt veszek gombra".
                     </dd>
 
                     <dt>Mikor lesznek a következő események?</dt>
@@ -99,48 +95,38 @@
                     </dd>
 
                     <dt>Hogyan törölhetem a fiókomat?</dt>
-                    <dd>Ha törölni szeretnéd a fiókodat, kérjük, vedd fel velünk a kapcsolatot e-mailben vagy az
-                        ügyfélszolgálati felületen keresztül.
+                    <dd>Ha törölni szeretnéd a fiókodat, kérjük, vedd fel velünk a kapcsolatot e-mailben,
+                        vagy keresd fel az adminisztrátorokat a továbbiakért
                         A kérés beérkezése után az adatokat biztonságosan töröljük a rendszerből.</dd>
 
                     <dt>Milyen adatokat gyűjtötök a felhasználókról?</dt>
-                    <dd>A felhasználókról a következő adatokat gyűjtjük: név, e-mail cím, lakcím (amennyiben megadod),
+                    <dd>A felhasználókról a következő adatokat gyűjtjük: név, e-mail cím, lakcím,
                         valamint a fiók kezeléséhez szükséges további információk.</dd>
 
                 </dl>
             </div>
         </div>
 
-        <!-- Hozzáférhetőség / Ügyfélszolgálat -->
+        <!-- Hozzáférhetőség -->
         <div class="card mb-3">
             <div id="helpHeader" class="card-header">
-                <h3>5. Hozzáférhetőség / Ügyfélszolgálat</h3>
+                <h3>5. Hozzáférhetőség</h3>
             </div>
             <div class="card-body">
                 <dl>
                     <dt>Hogyan érhetem el a fiókomat, ha elfelejtettem a jelszavamat?</dt>
-                    <dd>Ha elfelejtetted a jelszavadat, kattints a "Jelszó visszaállítása" gombra a bejelentkezési oldalon,
-                        és kövesd az utasításokat.
-                        E-mailben fogsz kapni egy linket, amely segítségével beállíthatod új jelszavadat. Ha további
-                        segítségre van szükséged,
-                        keresd fel ügyfélszolgálatunkat!</dd>
+                    <dd>Segítségért keresd fel az adminisztrátorokat!</dd>
 
                     <dt>Hogyan változtathatok a felhasználónevemen?</dt>
-                    <dd>Jelenleg a felhasználónevet nem lehet közvetlenül módosítani. Ha más felhasználónevet szeretnél,
-                        kérjük,
-                        töröld a fiókodat és hozz létre egy új regisztrációt az új felhasználónévvel.</dd>
+                    <dd>Jelenleg a felhasználónevet nem lehet közvetlenül módosítani.
+                        Ha más felhasználónevet szeretnél, keresd meg az adminisztrátorokat.
+                    </dd>
 
                     <dt>Mi történik, ha hibásan töltöm ki a regisztrációs űrlapot?</dt>
                     <dd>Ha hibás adatokat adsz meg a regisztráció során, az oldal értesíteni fog róla, hogy javítanod kell
                         azokat.
                         Kérjük, győződj meg arról, hogy minden mezőt helyesen töltöttél ki, mielőtt véglegesítenéd a
                         regisztrációt.</dd>
-
-                    <dt>Mi történik, ha többször is rossz jelszót adok meg?</dt>
-                    <dd>Ha többször is hibás jelszót adsz meg, biztonsági okokból ideiglenesen zároljuk a fiókodat.
-                        Ilyenkor a "Jelszó visszaállítása" opcióval új jelszót kérhetsz a fiókhoz. Ha problémát tapasztalsz,
-                        keresd fel ügyfélszolgálatunkat!</dd>
-
                 </dl>
             </div>
         </div>
